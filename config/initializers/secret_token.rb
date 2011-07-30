587e5cbc67db1d8641d8af2a9018edbd1121f435
Nathan Matthews <lowentropy@gmail.com>
2011-07-30 12:23:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resistance::Application.config.secret_token = 'c07566477790c3167d1155fcca34886c1f71721d0b0cd59272e5394d1d8205dc174ab2c22f4ed185332e389c6301ba152ba5f4467a41867c38ace74dc54416d8'
